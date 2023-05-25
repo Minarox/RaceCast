4a8cab01cabcbcea990503e6b5561531e31418ac
Mathis S.M <contact@minarox.fr>
2023-05-25 12:04:16 +0200

    Add Temperature component

---
+++ src/components/Temperature.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <Heading :title="$t('temperature')" />
+    <p>{{ temperature }} °C</p>
+  </section>
+</template>
+
+<script>
+import { state } from "@/socket";
+import Heading from "@/components/Heading.vue";
+
+export default {
+  name: "TemperatureComponent",
+  components: {
+    Heading,
+  },
+  computed: {
+    temperature() {
+      return state.temperature;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  align-items: center;
+  background-color: #f6f6f6;
+  border-radius: var(--border-radius);
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  overflow: hidden;
+  width: 260px;
+
+  > p {
+    font-size: 1.6em;
+    padding: 0.9rem;
+  }
+}
+</style>

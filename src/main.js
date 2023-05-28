46ff82e899715b0977414abb284451c515c113b7
Mathis S.M <contact@minarox.fr>
2023-05-28 19:28:58 +0200

    Add no data message

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import i18n from "./i18n";
 import Highcharts from "highcharts";
 import HighchartsVue from "highcharts-vue";
 import highchartsMore from "highcharts/highcharts-more";
+import highchartsNoData from "highcharts/modules/no-data-to-display";
 
 highchartsMore(Highcharts);
+highchartsNoData(Highcharts);
 createApp(App).use(i18n).use(HighchartsVue, { Highcharts }).mount("#app");

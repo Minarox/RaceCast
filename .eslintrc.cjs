97d33a6cbb783b025ab8237b1627ea60bd656a49
Minarox <contact@minarox.fr>
2023-08-23 12:54:09 +0200

    Add EsLint, Prettier and Sass

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+};

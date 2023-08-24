891c096d323645a8f021d31fdca4f00df5fb1c80
Minarox <contact@minarox.fr>
2023-08-24 03:30:38 +0200

    Create WebRTC component

---
+++ src/components/WebRTC.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export default {
+    name: "WebRTC",
+  };
+</script>
+
+<template>
+  <video autoplay controls muted playsinline></video>
+</template>
+
+<style lang="scss" scoped>
+  video {
+    bottom: 0;
+    height: 100%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+  }
+</style>

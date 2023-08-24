c96563236a4c7e01114787b8b9b209595768db5b
Minarox <contact@minarox.fr>
2023-08-24 03:31:00 +0200

    Create State component

---
+++ src/components/State.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { state } from "../socket";
+
+  export default {
+    name: "DataSate",
+    computed: {
+      state() {
+        return state.data.state;
+      },
+    },
+  };
+</script>
+
+<template>
+  <article>
+    <p :class="state.mpu6050 ? 'green' : 'red'">MPU6050</p>
+    <p :class="state.gps ? 'green' : 'red'">GPS</p>
+    <p :class="state.webrtc ? 'green' : 'red'">WebRTC</p>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+  article {
+    display: grid !important;
+    gap: 0.4rem;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    padding: 0.4rem;
+    width: calc(100% - 0.8rem) !important;
+
+    p {
+      border-radius: 4px;
+      color: white;
+      line-height: 1.8em;
+      text-align: center;
+      transition: background-color 300ms linear;
+      width: 100%;
+
+      &.green {
+        background-color: green;
+      }
+
+      &.red {
+        background-color: #d20000;
+      }
+    }
+  }
+</style>

0054a827ed20e3de5ea713d638b558d5e2b788ca
Minarox <contact@minarox.fr>
2023-08-24 03:32:15 +0200

    Update style, import components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <script lang="ts" setup>
-  import HelloWorld from "./components/HelloWorld.vue";
+  import SidePanel from "@/components/SidePanel.vue";
+  import WebRTC from "@/components/WebRTC.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img alt="Vite logo" class="logo" src="/favicon.svg" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img alt="Vue logo" class="logo vue" src="./assets/vue.svg" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <WebRTC />
+  <SidePanel />
 </template>
-
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    transition: filter 300ms;
-    will-change: filter;
-  }
-
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
-</style>

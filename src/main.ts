f77fc73fdcefa98ee8f84f9c5a379b714f669099
Minarox <contact@minarox.fr>
2023-08-27 22:32:23 +0200

    Add Highcharts package

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { registerSW } from "virtual:pwa-register";
+import Highcharts from "highcharts";
+import HighchartsVue from "highcharts-vue";
+import highchartsMore from "highcharts/highcharts-more";
+import highcharts3d from "highcharts/highcharts-3d";
+import highchartsNoData from "highcharts/modules/no-data-to-display";
 
 registerSW({ immediate: true });
 
-createApp(App).mount("#app");
+highchartsMore(Highcharts);
+highcharts3d(Highcharts);
+highchartsNoData(Highcharts);
+
+createApp(App).use(HighchartsVue, { Highcharts }).mount("#app");

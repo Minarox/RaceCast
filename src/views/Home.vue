2123182757ac0ff4f66580ac659dde3b4140807f
Minarox <contact@minarox.fr>
2023-09-07 03:57:44 +0200

    Add LiveKit SDK with router

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import SidePanel from "@/components/SidePanel.vue";
+  import Player from "@/components/Player.vue";
+
+  export default {
+    name: "HomePage",
+    components: {
+      SidePanel,
+      Player,
+    },
+    data() {
+      return {
+        webrtc: false as boolean,
+      };
+    },
+    methods: {
+      webrtcHandler(value: boolean) {
+        this.webrtc = value;
+      },
+    },
+  };
+</script>
+
+<template>
+  <Player @webrtc="webrtcHandler" />
+  <SidePanel :webrtc="webrtc" />
+</template>

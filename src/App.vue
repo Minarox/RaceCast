a667c8fc6c485ad4329cf51067136a7933696e13
Minarox <contact@minarox.fr>
2023-09-07 03:57:44 +0200

    Add LiveKit SDK with router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script lang="ts" setup>
-  import SidePanel from "@/components/SidePanel.vue";
-  import WebRTC from "@/components/WebRTC.vue";
-</script>
-
 <template>
-  <WebRTC />
-  <SidePanel />
+  <router-view />
 </template>
---
+++ src/components/Player.vue
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import { RemoteTrack, Room, RoomEvent, Track } from "livekit-client";
+
+  export default {
+    name: "PlayerComponent",
+    emits: { webrtc: null },
+    data() {
+      return {
+        play: false as boolean,
+        url: "wss://racecast-ix8aqrct.livekit.cloud" as string,
+        token: "" as string,
+      };
+    },
+    methods: {
+      requestToken() {
+        console.log("requesting token");
+        fetch(`https://rallye.minarox.fr/getToken`)
+          .then((res) => res.json())
+          .then((data) => {
+            this.token = data.token;
+            this.start();
+          });
+      },
+      async start() {
+        this.play = true;
+        this.clear();
+
+        // creates a new room with options
+        const room = new Room({
+          // automatically manage subscribed video quality
+          adaptiveStream: true,
+
+          // optimize publishing bandwidth and CPU for published tracks
+          dynacast: true,
+        });
+
+        // pre-warm connection, this can be called as early as your page is loaded
+        room.prepareConnection(this.url, this.token);
+
+        // set up event listeners
+        room
+          .on(RoomEvent.TrackSubscribed, this.handleTrackSubscribed)
+          .on(RoomEvent.TrackUnsubscribed, this.handleTrackUnsubscribed)
+          .on(RoomEvent.Disconnected, this.handleDisconnect);
+
+        // connect to room
+        await room.connect(this.url, this.token);
+        console.log("connected to room", room.name);
+      },
+      handleTrackSubscribed(track: RemoteTrack) {
+        if (
+          track.kind === Track.Kind.Video ||
+          track.kind === Track.Kind.Audio
+        ) {
+          // attach it to a new HTMLVideoElement or HTMLAudioElement
+          const element = track.attach();
+          document.getElementById("wrapper").appendChild(element);
+          // Check if video and audio is present
+          if (
+            document.getElementsByTagName("video").length > 0 &&
+            document.getElementsByTagName("audio").length > 0
+          )
+            this.$emit("webrtc", true);
+        }
+      },
+      handleTrackUnsubscribed(track: RemoteTrack) {
+        // remove tracks from all attached elements
+        track.detach();
+        this.clear();
+      },
+      handleDisconnect() {
+        this.play = false;
+        this.clear();
+        console.log("disconnected from room");
+      },
+      clear() {
+        document.getElementById("wrapper").innerHTML = "";
+        this.$emit("webrtc", false);
+      },
+    },
+  };
+</script>
+
+<template>
+  <button v-if="!play" @click.prevent="requestToken">Lancer le direct</button>
+  <div id="wrapper"></div>
+</template>
+
+<style lang="scss" scoped>
+  button {
+    left: 50%;
+    position: absolute;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 100;
+  }
+
+  #wrapper {
+    height: 100%;
+    width: 100%;
+  }
+</style>

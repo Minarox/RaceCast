953bb164eca8652ec823fd66a1977b4def02060c
Mathis S.M. <m.sema@exatech-group.com>
2024-05-14 12:23:57 +0200

    feat: Add compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import cloudflare from "@astrojs/cloudflare";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon()],
+    site: 'https://rallye.minarox.fr',
+    integrations: [icon(), playformCompress()],
     output: "hybrid",
     adapter: cloudflare({
-        imageService: "cloudflare",
+        imageService: "passthrough",
     }),
 });

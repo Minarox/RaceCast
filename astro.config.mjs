d554012764e82b78be3b488ef6a3ad2a6222cc33
Mathis S.M. <m.sema@exatech-group.com>
2024-05-14 14:18:31 +0200

    feat: Add metatags integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import cloudflare from "@astrojs/cloudflare";
 import playformCompress from "@playform/compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rallye.minarox.fr',
-  integrations: [icon(), robotsTxt(), sitemap(), playformCompress()],
+  integrations: [icon(),  metaTags(), robotsTxt(), sitemap(), playformCompress()],
   output: "hybrid",
   adapter: cloudflare({
     imageService: "passthrough"

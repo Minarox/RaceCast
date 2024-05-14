39a3457f0914491c5ea81ad2146fbe26ec54806e
Mathis S.M. <m.sema@exatech-group.com>
2024-05-14 16:44:20 +0200

    refactor: Remove compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
-import playformCompress from "@playform/compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
 import metaTags from "astro-meta-tags";
@@ -9,7 +8,7 @@ import metaTags from "astro-meta-tags";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rallye.minarox.fr',
-  integrations: [icon(),  metaTags(), robotsTxt(), sitemap(), playformCompress()],
+  integrations: [icon(), metaTags(), robotsTxt(), sitemap()],
   output: "hybrid",
   adapter: cloudflare({
     imageService: "passthrough"

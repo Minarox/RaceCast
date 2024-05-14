035b3f9fc7d304dca31ec0e101ce5bd8cd7603d4
Mathis S.M. <m.sema@exatech-group.com>
2024-05-14 12:29:16 +0200

    feat: Add robots-txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 import playformCompress from "@playform/compress";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rallye.minarox.fr',
-    integrations: [icon(), playformCompress()],
-    output: "hybrid",
-    adapter: cloudflare({
-        imageService: "passthrough",
-    }),
+  site: 'https://rallye.minarox.fr',
+  integrations: [icon(), robotsTxt(), playformCompress()],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "passthrough"
+  })
 });
